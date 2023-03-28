8b8731aa8dad3a91078ebb4aa11eaca64e47ebec
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-28 18:38:45 +0200

    React 94 - Práca s ikonkami DOPLNENIE

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { DiCompass } from "react-icons/di";
 
 const App = () => {
   return <div>
-            <h1>Naše první komponenta</h1>
+            <a href="https://react-icons.github.io/react-icons/icons?name=di" target="_blank">odkaz na ikony</a>
+            <p className="popis">Otvoríme odkaz, skopírujeme import a potom meníme len názov ikony </p>
             <Fa500Px className="my-icon"/>
             <DiCompass  className="my-icon"/>
         </div>
