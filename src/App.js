686d18b10aa6668029de9ba2f39fbded033c8d68
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-28 18:42:09 +0200

    95. React - Projekt slider - ukážka

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-//React 94 - Práca s ikonkami
+//95. React - Projekt slider - ukážka
 
-import { Fa500Px } from "react-icons/fa";
-import { DiCompass } from "react-icons/di";
+
 
 const App = () => {
   return <div>
-            <a href="https://react-icons.github.io/react-icons/icons?name=di" target="_blank">odkaz na ikony</a>
-            <p className="popis">Otvoríme odkaz, skopírujeme import a potom meníme len názov ikony </p>
-            <Fa500Px className="my-icon"/>
-            <DiCompass  className="my-icon"/>
+           
         </div>
   
   
